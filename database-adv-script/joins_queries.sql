a041db28b4cc0a810657968f9ab9c79fed78d772
samiyonas <samuel.x.yonas@gmail.com>
2025-09-18 01:01:38 +0300

    fixed joins_queries.sql

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -2,7 +2,8 @@ SELECT Booking.* User.* FROM Booking
 INNER JOIN User ON Booking.user_id = User.user_id;
 
 SELECT Property.* Review.* FROM Property
-LEFT JOIN Review ON Property.property_id = Review.property_id;
+LEFT JOIN Review ON Property.property_id = Review.property_id
+ORDER BY Review.rating DESC;
 
 SELECT User.* Booking.* FROM User
 FULL OUTER JOIN Booking ON User.user_id = Booking.user_id;
