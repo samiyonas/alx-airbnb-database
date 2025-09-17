6a873cc88452d638d177d423533079c355c5af87
samiyonas <samuel.x.yonas@gmail.com>
2025-09-18 01:55:57 +0300

    fixed subqueries.sql

--- database-adv-script/subqueries.sql
+++ database-adv-script/subqueries.sql
@@ -1,2 +1,12 @@
 SELECT * FROM Property
-WHERE rating > (SELECT AVG(rating) FROM Property);
+WHERE property_id IN (
+    SELECT property_id FROM Review
+    GROUP BY property_id
+    HAVING AVG(rating) > 4.0
+);
+
+SELECT * FROM User u
+WHERE (
+    SELECT COUNT(*) FROM Booking b
+    WHERE u.user_id = b.user_id
+) > 3;
