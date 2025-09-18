3c936b795886ae497f645365b45936664e057543
samiyonas <samuel.x.yonas@gmail.com>
2025-09-18 05:13:26 +0300

    benchmark code

--- database-adv-script/database_index.sql
+++ database-adv-script/database_index.sql
@@ -1,2 +1,36 @@
+EXPLAIN ANALYZE
+SELECT
+    p.*,
+    RANK() OVER(
+        ORDER BY (
+            SELECT COUNT(*) FROM Booking
+            WHERE p.property_id = Booking.property_id
+        ) DESC
+    ) AS ra,
+    ROW_NUMBER() OVER(
+        ORDER BY (
+            SELECT COUNT(*) FROM Booking
+            WHERE p.property_id = Booking.property_id
+        ) DESC
+    ) AS row_num
+FROM Property p;
+
 CREATE INDEX property_id_index ON Property (property_id);
 CREATE INDEX booking_property_id_index ON Booking (property_id);
+
+EXPLAIN ANALYZE
+SELECT
+    p.*,
+    RANK() OVER(
+        ORDER BY (
+            SELECT COUNT(*) FROM Booking
+            WHERE p.property_id = Booking.property_id
+        ) DESC
+    ) AS ra,
+    ROW_NUMBER() OVER(
+        ORDER BY (
+            SELECT COUNT(*) FROM Booking
+            WHERE p.property_id = Booking.property_id
+        ) DESC
+    ) AS row_num
+FROM Property p;
